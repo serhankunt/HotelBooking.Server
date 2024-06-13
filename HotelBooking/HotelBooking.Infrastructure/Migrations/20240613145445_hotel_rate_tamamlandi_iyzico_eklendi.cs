d85d3c12d2d9443cbb12f4aefc67ce897b0259ee
serhankunt <hserhan006@gmail.com>
2024-06-13 17:58:21 +0300

    Added migration.

---
+++ HotelBooking/HotelBooking.Infrastructure/Migrations/20240613145445_hotel_rate_tamamlandi_iyzico_eklendi.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelBooking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class hotel_rate_tamamlandi_iyzico_eklendi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
